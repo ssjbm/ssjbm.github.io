534a892e3fa6465eb5468e1af1fe564825370768
Société Saint-Jean Baptiste de Montréal <mlroy@ssjb.com>
2025-08-11 12:01:14 -0400

    Stats part 1

---
+++ stats/_index.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @type     article
+ * @title    Statistiques générales
+ * @icon     images/icon.svg
+ * @abstract Statistiques du site web et des réseaux sociaux
+ */
+?>
+
+<grostitre>Facebook</grostitre>
+
+<doclink href="./reports/visites.html">Statistiques de visites</doclink>
+<br>
+<doclink href="./reports/interactions.html">Statistiques d'interactions</doclink>
+<br>
+<doclink href="./reports/portee.html">Statistiques de portée</doclink>
+
+
+<h3>Contenus les plus populaires</h3>
+
+<img src="./images/contents-1.png" alt="">
+<img src="./images/contents-2.png" alt="">
+
+
+<h3>Audience</h3>
+
+<img src="./images/audience.png" alt="">
+
+
+<h3>Formats</h3>
+
+<img src="./images/formats.png" alt="">
+
+
+<grostitre>Site web</grostitre>
+
+
+
+
