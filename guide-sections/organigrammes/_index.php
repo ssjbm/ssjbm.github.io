cb9e8f58f28640cfeff27955f2dd19e7c079525f
Maxime Larrivée-Roy <zmotrin@gmail.com>
2025-08-13 20:12:18 -0400

    Forward and updates

--- guide-sections/organigrammes/_index.php
+++ guide-sections/organigrammes/_index.php
@@ -10,17 +10,237 @@
 
 <grostitre>Structure Politique</grostitre>
 
-<br>
+<br><br>
 
-<timg src="./images/structure-politique-$t.svg"></timg>
+<svg style="background:0 0;background-color:transparent;user-select: none;" viewBox="-.5 -.5 601 421">
+  <g stroke="currentColor" stroke-miterlimit="10">
+    <path fill="none" d="M300 60v53.6" pointer-events="stroke" style="stroke:currentColor"/>
+    <path d="m300 118.9-3.5-7 3.5 1.7 3.5-1.7Z" pointer-events="all" style="fill:currentColor;stroke:currentColor"/>
+  </g>
+  <path fill="transparent" stroke="currentColor" d="M240 0h120v60H240z" pointer-events="all" style="stroke:currentColor"/>
+  <switch transform="translate(-.5 -.5)">
+    <foreignObject width="100%" height="100%" pointer-events="none" requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility" style="overflow:visible;text-align:left">
+      <div style="display:flex;align-items:unsafe center;justify-content:unsafe center;width:118px;height:1px;padding-top:30px;margin-left:241px">
+        <div style="box-sizing:border-box;font-size:0;text-align:center;color:currentColor">
+          <div style="display:inline-block;font-size:12px;font-family:var(--font);color:currentColor;line-height:1.2;pointer-events:all;white-space:normal;word-wrap:normal">
+            Assemblée Générale Délégué.e.s
+          </div>
+        </div>
+      </div>
+    </foreignObject>
+  </switch>
+  <g stroke="currentColor" stroke-miterlimit="10">
+    <path fill="none" d="M300 180v53.6" pointer-events="stroke" style="stroke:currentColor"/>
+    <path d="m300 238.9-3.5-7 3.5 1.7 3.5-1.7Z" pointer-events="all" style="fill:currentColor;stroke:currentColor"/>
+  </g>
+  <path fill="transparent" stroke="currentColor" d="M240 120h120v60H240z" pointer-events="all" style="stroke:currentColor"/>
+  <switch transform="translate(-.5 -.5)">
+    <foreignObject width="100%" height="100%" pointer-events="none" requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility" style="overflow:visible;text-align:left">
+      <div style="display:flex;align-items:unsafe center;justify-content:unsafe center;width:118px;height:1px;padding-top:150px;margin-left:241px">
+        <div style="box-sizing:border-box;font-size:0;text-align:center;color:currentColor">
+          <div style="display:inline-block;font-size:12px;font-family:var(--font);color:currentColor;line-height:1.2;pointer-events:all;white-space:normal;word-wrap:normal">
+            Assemblée Générale Délégué.e.s
+          </div>
+        </div>
+      </div>
+    </foreignObject>
+  </switch>
+  <path fill="transparent" stroke="currentColor" d="M480 130h120v60H480z" pointer-events="all" style="stroke:currentColor"/>
+  <switch transform="translate(-.5 -.5)">
+    <foreignObject width="100%" height="100%" pointer-events="none" requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility" style="overflow:visible;text-align:left">
+      <div style="display:flex;align-items:unsafe center;justify-content:unsafe center;width:118px;height:1px;padding-top:160px;margin-left:481px">
+        <div style="box-sizing:border-box;font-size:0;text-align:center;color:currentColor">
+          <div style="display:inline-block;font-size:12px;font-family:var(--font);color:currentColor;line-height:1.2;pointer-events:all;white-space:normal;word-wrap:normal">
+            Comité de vérification des finances
+          </div>
+        </div>
+      </div>
+    </foreignObject>
+  </switch>
+  <g stroke="currentColor" stroke-miterlimit="10">
+    <path fill="none" d="M360 30h170q10 0 10 10v83.6" pointer-events="stroke" style="stroke:currentColor"/>
+    <path d="m540 128.9-3.5-7 3.5 1.7 3.5-1.7Z" pointer-events="all" style="fill:currentColor;stroke:currentColor"/>
+  </g>
+  <g stroke="currentColor" stroke-miterlimit="10">
+    <path fill="none" d="M300 300v53.6" pointer-events="stroke" style="stroke:currentColor"/>
+    <path d="m300 358.9-3.5-7 3.5 1.7 3.5-1.7Z" pointer-events="all" style="fill:currentColor;stroke:currentColor"/>
+  </g>
+  <g stroke="currentColor" stroke-miterlimit="10">
+    <path fill="none" d="M240 270H70q-10 0-10 10v73.6" pointer-events="stroke" style="stroke:currentColor"/>
+    <path d="m60 358.9-3.5-7 3.5 1.7 3.5-1.7Z" pointer-events="all" style="fill:currentColor;stroke:currentColor"/>
+  </g>
+  <g stroke="currentColor" stroke-miterlimit="10">
+    <path fill="none" d="M360 270h170q10 0 10 10v73.6" pointer-events="stroke" style="stroke:currentColor"/>
+    <path d="m540 358.9-3.5-7 3.5 1.7 3.5-1.7Z" pointer-events="all" style="fill:currentColor;stroke:currentColor"/>
+  </g>
+  <path fill="transparent" stroke="currentColor" d="M240 240h120v60H240z" pointer-events="all" style="stroke:currentColor"/>
+  <switch transform="translate(-.5 -.5)">
+    <foreignObject width="100%" height="100%" pointer-events="none" requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility" style="overflow:visible;text-align:left">
+      <div style="display:flex;align-items:unsafe center;justify-content:unsafe center;width:118px;height:1px;padding-top:270px;margin-left:241px">
+        <div style="box-sizing:border-box;font-size:0;text-align:center;color:currentColor">
+          <div style="display:inline-block;font-size:12px;font-family:var(--font);color:currentColor;line-height:1.2;pointer-events:all;white-space:normal;word-wrap:normal">
+            Comité de Direction
+            <br/>
+            Nommé.e.s
+          </div>
+        </div>
+      </div>
+    </foreignObject>
+  </switch>
+  <path fill="transparent" stroke="currentColor" d="M240 360h120v60H240z" pointer-events="all" style="stroke:currentColor"/>
+  <switch transform="translate(-.5 -.5)">
+    <foreignObject width="100%" height="100%" pointer-events="none" requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility" style="overflow:visible;text-align:left">
+      <div style="display:flex;align-items:unsafe center;justify-content:unsafe center;width:118px;height:1px;padding-top:390px;margin-left:241px">
+        <div style="box-sizing:border-box;font-size:0;text-align:center;color:currentColor">
+          <div style="display:inline-block;font-size:12px;font-family:var(--font);color:currentColor;line-height:1.2;pointer-events:all;white-space:normal;word-wrap:normal">
+            Comités conjoints
+          </div>
+        </div>
+      </div>
+    </foreignObject>
+  </switch>
+  <path fill="transparent" stroke="currentColor" d="M0 360h120v60H0z" pointer-events="all" style="stroke:currentColor"/>
+  <switch transform="translate(-.5 -.5)">
+    <foreignObject width="100%" height="100%" pointer-events="none" requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility" style="overflow:visible;text-align:left">
+      <div style="display:flex;align-items:unsafe center;justify-content:unsafe center;width:118px;height:1px;padding-top:390px;margin-left:1px">
+        <div style="box-sizing:border-box;font-size:0;text-align:center;color:currentColor">
+          <div style="display:inline-block;font-size:12px;font-family:var(--font);color:currentColor;line-height:1.2;pointer-events:all;white-space:normal;word-wrap:normal">
+            Comission des présidentes et présidents
+          </div>
+        </div>
+      </div>
+    </foreignObject>
+  </switch>
+  <path fill="transparent" stroke="currentColor" d="M480 360h120v60H480z" pointer-events="all" style="stroke:currentColor"/>
+  <switch transform="translate(-.5 -.5)">
+    <foreignObject width="100%" height="100%" pointer-events="none" requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility" style="overflow:visible;text-align:left">
+      <div style="display:flex;align-items:unsafe center;justify-content:unsafe center;width:118px;height:1px;padding-top:390px;margin-left:481px">
+        <div style="box-sizing:border-box;font-size:0;text-align:center;color:currentColor">
+          <div style="display:inline-block;font-size:12px;font-family:var(--font);color:currentColor;line-height:1.2;pointer-events:all;white-space:normal;word-wrap:normal">
+            Conseil Jeunesse
+          </div>
+        </div>
+      </div>
+    </foreignObject>
+  </switch>
+</svg>
 
-<dots></dots>
-<br>
+<dots></dots><br>
+
 
 <grostitre>Structure Adminsitrative</grostitre>
 
-<br>
+<br><br>
 
-<timg src="./images/structure-administrative-$t.svg"></timg>
+<svg style="background:0 0;background-color:transparent; user-select: none;" viewBox="-.5 -.5 601 421">
+  <g stroke="currentColor" stroke-miterlimit="10">
+    <path fill="none" d="M300 60v53.6" pointer-events="stroke" style="stroke:currentColor"/>
+    <path d="m300 118.9-3.5-7 3.5 1.7 3.5-1.7Z" pointer-events="all" style="fill:currentColor;stroke:currentColor"/>
+  </g>
+  <path fill="transparent" stroke="currentColor" d="M240 0h120v60H240z" pointer-events="all" style="stroke:currentColor"/>
+  <switch transform="translate(-.5 -.5)">
+    <foreignObject width="100%" height="100%" pointer-events="none" requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility" style="overflow:visible;text-align:left">
+      <div style="display:flex;align-items:unsafe center;justify-content:unsafe center;width:118px;height:1px;padding-top:30px;margin-left:241px">
+        <div style="box-sizing:border-box;font-size:0;text-align:center;color:currentColor">
+          <div style="display:inline-block;font-size:12px;font-family:var(--font);color:currentColor;line-height:1.2;pointer-events:all;white-space:normal;word-wrap:normal">
+            Conseil Général
+          </div>
+        </div>
+      </div>
+    </foreignObject>
+  </switch>
+  <g stroke="currentColor" stroke-miterlimit="10">
+    <path fill="none" d="M300 180v53.6" pointer-events="stroke" style="stroke:currentColor"/>
+    <path d="m300 238.9-3.5-7 3.5 1.7 3.5-1.7Z" pointer-events="all" style="fill:currentColor;stroke:currentColor"/>
+  </g>
+  <path fill="transparent" stroke="currentColor" d="M240 120h120v60H240z" pointer-events="all" style="stroke:currentColor"/>
+  <switch transform="translate(-.5 -.5)">
+    <foreignObject width="100%" height="100%" pointer-events="none" requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility" style="overflow:visible;text-align:left">
+      <div style="display:flex;align-items:unsafe center;justify-content:unsafe center;width:118px;height:1px;padding-top:150px;margin-left:241px">
+        <div style="box-sizing:border-box;font-size:0;text-align:center;color:currentColor">
+          <div style="display:inline-block;font-size:12px;font-family:var(--font);color:currentColor;line-height:1.2;pointer-events:all;white-space:normal;word-wrap:normal">
+            Présidence
+          </div>
+        </div>
+      </div>
+    </foreignObject>
+  </switch>
+  <g stroke="currentColor" stroke-miterlimit="10">
+    <path fill="none" d="M330 300v20q0 10 10 10h30q10 0 10 10v13.6" pointer-events="stroke" style="stroke:currentColor"/>
+    <path d="m380 358.9-3.5-7 3.5 1.7 3.5-1.7Z" pointer-events="all" style="fill:currentColor;stroke:currentColor"/>
+  </g>
+  <g stroke="currentColor" stroke-miterlimit="10">
+    <path fill="none" d="M360 270h170q10 0 10 10v73.6" pointer-events="stroke" style="stroke:currentColor"/>
+    <path d="m540 358.9-3.5-7 3.5 1.7 3.5-1.7Z" pointer-events="all" style="fill:currentColor;stroke:currentColor"/>
+  </g>
+  <g stroke="currentColor" stroke-miterlimit="10">
+    <path fill="none" d="M240 270H70q-10 0-10 10v73.6" pointer-events="stroke" style="stroke:currentColor"/>
+    <path d="m60 358.9-3.5-7 3.5 1.7 3.5-1.7Z" pointer-events="all" style="fill:currentColor;stroke:currentColor"/>
+  </g>
+  <path fill="transparent" stroke="currentColor" d="M240 240h120v60H240z" pointer-events="all" style="stroke:currentColor"/>
+  <switch transform="translate(-.5 -.5)">
+    <foreignObject width="100%" height="100%" pointer-events="none" requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility" style="overflow:visible;text-align:left">
+      <div style="display:flex;align-items:unsafe center;justify-content:unsafe center;width:118px;height:1px;padding-top:270px;margin-left:241px">
+        <div style="box-sizing:border-box;font-size:0;text-align:center;color:currentColor">
+          <div style="display:inline-block;font-size:12px;font-family:var(--font);color:currentColor;line-height:1.2;pointer-events:all;white-space:normal;word-wrap:normal">
+            Direction Générale
+          </div>
+        </div>
+      </div>
+    </foreignObject>
+  </switch>
+  <path fill="transparent" stroke="currentColor" d="M160 360h120v60H160z" pointer-events="all" style="stroke:currentColor"/>
+  <switch transform="translate(-.5 -.5)">
+    <foreignObject width="100%" height="100%" pointer-events="none" requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility" style="overflow:visible;text-align:left">
+      <div style="display:flex;align-items:unsafe center;justify-content:unsafe center;width:118px;height:1px;padding-top:390px;margin-left:161px">
+        <div style="box-sizing:border-box;font-size:0;text-align:center;color:currentColor">
+          <div style="display:inline-block;font-size:12px;font-family:var(--font);color:currentColor;line-height:1.2;pointer-events:all;white-space:normal;word-wrap:normal">
+            Chargé de projets et agent de liaison
+          </div>
+        </div>
+      </div>
+    </foreignObject>
+  </switch>
+  <path fill="transparent" stroke="currentColor" d="M320 360h120v60H320z" pointer-events="all" style="stroke:currentColor"/>
+  <switch transform="translate(-.5 -.5)">
+    <foreignObject width="100%" height="100%" pointer-events="none" requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility" style="overflow:visible;text-align:left">
+      <div style="display:flex;align-items:unsafe center;justify-content:unsafe center;width:118px;height:1px;padding-top:390px;margin-left:321px">
+        <div style="box-sizing:border-box;font-size:0;text-align:center;color:currentColor">
+          <div style="display:inline-block;font-size:12px;font-family:var(--font);color:currentColor;line-height:1.2;pointer-events:all;white-space:normal;word-wrap:normal">
+            Agent de bâtiment et aide au projets
+          </div>
+        </div>
+      </div>
+    </foreignObject>
+  </switch>
+  <path fill="transparent" stroke="currentColor" d="M480 360h120v60H480z" pointer-events="all" style="stroke:currentColor"/>
+  <switch transform="translate(-.5 -.5)">
+    <foreignObject width="100%" height="100%" pointer-events="none" requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility" style="overflow:visible;text-align:left">
+      <div style="display:flex;align-items:unsafe center;justify-content:unsafe center;width:118px;height:1px;padding-top:390px;margin-left:481px">
+        <div style="box-sizing:border-box;font-size:0;text-align:center;color:currentColor">
+          <div style="display:inline-block;font-size:12px;font-family:var(--font);color:currentColor;line-height:1.2;pointer-events:all;white-space:normal;word-wrap:normal">
+            Agent de bâtiment
+          </div>
+        </div>
+      </div>
+    </foreignObject>
+  </switch>
+  <path fill="transparent" stroke="currentColor" d="M0 360h120v60H0z" pointer-events="all" style="stroke:currentColor"/>
+  <switch transform="translate(-.5 -.5)">
+    <foreignObject width="100%" height="100%" pointer-events="none" requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility" style="overflow:visible;text-align:left">
+      <div style="display:flex;align-items:unsafe center;justify-content:unsafe center;width:118px;height:1px;padding-top:390px;margin-left:1px">
+        <div style="box-sizing:border-box;font-size:0;text-align:center;color:currentColor">
+          <div style="display:inline-block;font-size:12px;font-family:var(--font);color:currentColor;line-height:1.2;pointer-events:all;white-space:normal;word-wrap:normal">
+            Adjointe administrative
+          </div>
+        </div>
+      </div>
+    </foreignObject>
+  </switch>
+  <g stroke="currentColor" stroke-miterlimit="10">
+    <path fill="none" d="M270 300v20q0 10-10 10h-30q-10 0-10 10v13.6" pointer-events="stroke" style="stroke:currentColor"/>
+    <path d="m220 358.9-3.5-7 3.5 1.7 3.5-1.7Z" pointer-events="all" style="fill:currentColor;stroke:currentColor"/>
+  </g>
+</svg>
 
 <dots></dots>
