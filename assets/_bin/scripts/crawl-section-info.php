61cc456ccefdfab23239b25234795aa117348efb
Société Saint-Jean Baptiste de Montréal <mlroy@ssjb.com>
2025-08-21 16:53:06 -0400

    refactor section id

--- assets/_bin/scripts/crawl-section-info.php
+++ assets/_bin/scripts/crawl-section-info.php
@@ -3,24 +3,24 @@
 // Load PXDoc utilities
 require(__DIR__ . '/../../../pxdoc/_bin/scripts/utils.php');
 
+
+// Initialize contents
 $srcMapDir = realpath(__DIR__ . '../../../maps'). S;
 if(!$rulesFile = realpath($srcMapDir . 'sections.json')) err("Can't find rules file.");
 if(!$rules = json_decode(file_get_contents($rulesFile))) err("Invalid rules file.");
 $rules->sections[] = $rules->defaultSection;
 
 
-
+// Crawl each sections for infos
 foreach($rules->sections as $section) {
+    echo 'Crawl: ' . $section->name . '...' . RN;
     $page = new Scraper($section->url);
     
-    echo $section->name.RN;
     $section->description = trim($page->query('//section[1]/div/div[2]/div/div/div/div/section[2]')->item(0)->textContent);
 
+    $infos = new stdClass;
     $results = $page->query('//section[3]/div//table');
     if(!$results->length) $results = $page->query('//section[4]/div//table');
-    // $table = $;
-
-    // $rows = $page->query($results->item(0)->getNodePath() . '//tr');
     $table = $results->item(0);
     foreach($table->getElementsByTagName('tr') as $row) {
         $key = $row->getElementsByTagName('td')->item(0)->textContent;
@@ -28,21 +28,36 @@ foreach($rules->sections as $section) {
         $key = str_normalize($key);
         $key = strtolower($key);
         $key = trim(preg_replace('#[^\w]+#i', '_', $key), '_');
+        if(!$key) continue;
 
-        echo $key.RN;
+        $values = [];
+        $cell = $row->getElementsByTagName('td')->item(1);
+        $paragraphs = $cell->getElementsByTagName('p');
+        if($paragraphs->length) {
+            foreach($paragraphs as $parag){
+                $value = trim($parag->textContent);
+                if($value) $values[] = $value;
+            }
+        } else {
+            foreach($cell->childNodes as $elm) {
+                if($elm->nodeType !== XML_TEXT_NODE) continue;
+                if(($value = trim($elm->textContent))) $values[] = $value;
+            }
+        }
 
+        // Cleanup
+        foreach($values as $k => $v) if(!trim($v,' ')) unset($values[$k]);
+        $infos->{$key} = $values;
+        $section->id = preg_replace('#[^\w]+#i', '-', $section->id);
     }
+    $section->infos = $infos;
 }
 
 
-
-// $index = new Scraper('https://ssjb.com/sections/');
-
-// foreach($index->query('//div[@class="elementor-widget-container"]/ul/li/*/a') as $secElm) {
-//     $section
-//     $text = trim($secElm->textContent);
-//     // echo $secElm->getNodePath().RN;
-
-// }
+// Save the new result
+array_pop($rules->sections);
+file_put_contents($rulesFile, json_encode($rules, JSON_PRETTY_PRINT));
 
 
+// EN FRANÇAIS!
+echo RN . 'EN FRANÇAIS ✊' . RN;
