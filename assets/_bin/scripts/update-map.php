f1f0dc00f68eb9f7b0baf1dd01c935fedd18cae7
Maxime Larrivée-Roy <zmotrin@gmail.com>
2025-08-23 18:35:24 -0400

    Pauffinage du sort des sections

--- assets/_bin/scripts/update-map.php
+++ assets/_bin/scripts/update-map.php
@@ -104,7 +104,7 @@ file_put_contents($sectionsFile, json_encode($sections));
 // Sort sections for palette intersection
 echo "Sort sections for palette intersection..." . RN;
 $sections = json_decode(file_get_contents($sectionsFile));
-$orderedIds = FeatureOrder::orderIds($geoSectionFile,['idKeys' => ['id'], 'seed'   => 'barycenter', 'metric' => 'euclid', 'step' => 6]);
+$orderedIds = FeatureOrder::orderIds($geoSectionFile,['idKeys' => ['id'], 'seed'   => 'barycenter', 'metric' => 'euclid', 'step' => 8]);
 foreach($sections->sections as $section) $section->color = array_search($section->id, $orderedIds);
 file_put_contents($sectionsFile, json_encode($sections));
 
